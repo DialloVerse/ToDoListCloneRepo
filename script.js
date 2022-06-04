fa7d2c71b5e68d7123988b9ebbbd4947ac553694
DialloVerse <durali@ymail.com>
2022-06-04 10:28:39 +0200

    completed JscriptFixes

--- script.js
+++ script.js
@@ -31,14 +31,56 @@ function createTodo() {
   addInput.value = "";
 }
 
+function showEditInput(paragraphElement) {
+  var editInput = document.getElementsByName("editInput")[0];
+  if (editInput) {
+    editInput.remove();
+  }
+
+  var input = document.createElement("input");
+  input.type = "text";
+  input.name = "editInput";
+  input.value = paragraphElement.textContent;
+  input.classList.add("editInput");
+
+  paragraphElement.parentElement.appendChild();
+  input.focus();
+}
+
+function updateTodo() {
+  var editInput = document.getElementById("editInput")[0];
+  if (!editInput) {
+    return;
+  }
+
+  var newText = editInput.value;
+
+  if (newText !== "") {
+    var paragraph = editInput.parentElement.querySelector(".paragraph");
+    paragraph.textContent = newText;
+  }
+
+  editInput.remove();
+}
+
 function removeTodo(removeElement) {
   removeElement.parentElement.remove();
 }
 
+function toggleComplete() {
+  if (inputElement.checked === false) {
+    inputElement.parentElement.classList.remove("complete");
+  } else {
+    inputElement.parentElement.classList.add("complete");
+  }
+}
+
 list.addEventListener("click", function (event) {
+  event.stopPropagation();
+
   switch (event.target.tagName) {
     case "p":
-      showEditInput();
+      showEditInput(event.target);
       break;
     case "SPAN":
       removeTodo(event.target);
@@ -52,7 +94,22 @@ list.addEventListener("change", function (event) {
   }
 });
 
-addBtn.addEventListener("click", createTodo);
+list.addEventListener("keypress", function (event) {
+  if (
+    event.target.tagName === "INPUT" &&
+    event.target.type === "text" &&
+    event.key === "Enter"
+  ) {
+    updateTodo();
+  }
+});
+
+document.addEventListener("click", updateTodo);
+
+addBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  createTodo();
+});
 
 addInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
